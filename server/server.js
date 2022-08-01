2dd4ecfa5766e8a49ae07b550162473cd1213036
Anita Chengalva <anita.chengalva@gmail.com>
2022-07-31 23:41:14 -0700

    updated dependancies and created new apollo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// new Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
