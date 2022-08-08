1aa9e673dbab02ad06564dd46aaf2405b69300c7
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-08 13:21:12 -0700

    updated resolvers query me

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,10 +9,13 @@ const resolvers = {
       return User.findOne({ _id: userId });
     },
 
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOne({ _id: context.user._id });
-      }
+    me: async (_parent, _args, context) => {
+        if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password');
+  
+            return userData;
+        }
       throw new AuthenticationError("No user found. Please login or register");
     },
   },
