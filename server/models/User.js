fc738106f3da77b7e0ca2cba3aef9c8b15d007d9
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-08 12:54:34 -0700

    removed unnecessary comments and code

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,6 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// import schema from Book.js
 const bookSchema = require('./Book');
 
 const userSchema = new Schema(
@@ -21,10 +20,8 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    // set savedBooks to be an array of data that adheres to the bookSchema
     savedBooks: [bookSchema],
   },
-  // set this to use virtual below
   {
     toJSON: {
       virtuals: true,
@@ -47,7 +44,7 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+// `bookCount` field with the number of saved books
 userSchema.virtual('bookCount').get(function () {
   return this.savedBooks.length;
 });
