5b381a6efe5f4931ae01ab637e7d7af5ec979ee9
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-08 22:17:41 -0700

    creating graphql endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,26 @@ import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
+// GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <Router>
